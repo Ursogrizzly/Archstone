ff49c6c511854f339c20e2dfdfba6b76492e2897
André Felipe Araujo Martins <andrearaujofm@gmail.com>
2016-05-30 13:28:10 -0700

    test

--- src/Attributes.java
+++ src/Attributes.java
@@ -1,12 +1,15 @@
 class Attributes {
 	
 	int Vitality = 0;
-	int Endurance = 0;
 	int Strength = 0;
 	int Dexterity = 0;
 	int Intelligence = 0;
 	int Luck = 0;
 	int test =0;
+}
+
+class AttributeEffect extends Attributes {
+	
 	
 }
 
@@ -21,10 +24,9 @@ class Status {
 	Burden = 0;
 }
 
-class currentEquipment {
+class currentEquipment extends Status {
 
 	Weapon = 0;
 	Shield = 0;
-	Armor = 0;
-	
+	Armor = 0;	
 }
