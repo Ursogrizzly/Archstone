661f82671c141f00e428267de9c474c06295f908
connerward <connerward@conners-mbp.lan>
2016-05-30 14:39:52 -0700

    test

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,14 @@
-
+/*
+ * Program 
+ */
+import java.util.Scanner;
 public class Game {
-
+	public static void main(String[] args) {
+		Scanner keyboard;
+		keyboard = new Scanner(System.in);
+		int userWeight;
+		System.out.print("What is your weight in kilograms?");
+		userWeight = (int) keyboard.nextDouble();
+		keyboard.close();
+	}
 }
