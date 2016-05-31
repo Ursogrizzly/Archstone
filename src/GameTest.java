cf445e12f2d8d66da8edbaa179cf853e48d684bc
connerward <connerward@conners-mbp.lan>
2016-05-30 17:57:59 -0700

    test

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,3 +1,6 @@
+/**
+ * Program 
+ */
 import java.util.Scanner;
 public class GameTest {
 	public static void main(String[] args) {
@@ -14,12 +17,8 @@ public class GameTest {
         System.out.println("Player Weight Status: "); 
         System.out.println(Player1.getPlayerWeightStaus() + "\n"); //for testing purposes shows status in words
         
-        Player1.setPlayerMaxHealth(100); // sets max health 
-        Player1.setPlayerCurrentHealth(100); //set current health 
-        System.out.println("Your current health points are " + Player1.getPlayerCurrentHealth()); //gets current health from instance of PlayerStats
-        System.out.println("of a max health points of " + Player1.getPlayerMaxHealth()); //gets Max health from instance of PlayerStats
-        System.out.println("Which is " + Player1.getPlayerHealthStatus()+ "%" + "\n"); //gets health status ((Current/Max)*100 + a percentage sign) from instance of PlayerStats
-       
+
+        
         System.out.println("Pick up sword with 12 weight? True or False");
         boolean sword = keyboard.nextBoolean();// for testing
         double swordWeight = 12; //sword weight for testing, not sure how to implement
@@ -30,3 +29,11 @@ public class GameTest {
 		keyboard.close();
 	}
 }
+
+/*
+Player1.setPlayerMaxHealth(100); // sets max health 
+Player1.setPlayerCurrentHealth(100); //set current health 
+System.out.println("Your current health points are " + Player1.getPlayerCurrentHealth()); //gets current health from instance of PlayerStats
+System.out.println("of a max health points of " + Player1.getPlayerMaxHealth()); //gets Max health from instance of PlayerStats
+System.out.println("Which is " + Player1.getPlayerHealthStatus()+ "%" + "\n"); //gets health status ((Current/Max)*100 + a percentage sign) from instance of PlayerStats
+*/
