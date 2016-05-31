3c2058b46f78a32352c2347751e26fe491949645
connerkward <conner.k.ward@gmail.com>
2016-05-30 17:53:09 -0700

    Update GameTest.java

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 public class GameTest {
 	public static void main(String[] args) {
-		Scanner keyboard;
+		Scanner keyboard; //creates the new Scanner Object 
 		keyboard = new Scanner(System.in);
 		
-		PlayerStats Player1;
+		PlayerStats Player1;//creates a new player that will hold current and max carry weights and health
 		Player1 = new PlayerStats();
 		
 		System.out.println("What is your Carry Limit?");
-		Player1.setPlayerCarryLimit((double) keyboard.nextDouble());
+		Player1.setPlayerCarryLimit((double) keyboard.nextDouble());//user input sets carry limit
 		System.out.println("What is your Carry Weight?");
-        Player1.setPlayerCarryWeight((double) keyboard.nextDouble());
-        System.out.println("Player Weight Status: ");
-        System.out.println(Player1.getPlayerWeightStaus() + "\n");
+        Player1.setPlayerCarryWeight((double) keyboard.nextDouble());//user input sets carry weight
+        System.out.println("Player Weight Status: "); 
+        System.out.println(Player1.getPlayerWeightStaus() + "\n"); //for testing purposes shows status in words
         
-        Player1.setPlayerMaxHealth(100);
-        Player1.setPlayerCurrentHealth(100);
-        System.out.println("Your current health points are " + Player1.getPlayerCurrentHealth());
-        System.out.println("of a max health points of " + Player1.getPlayerMaxHealth());
-        System.out.println("Which is " + Player1.getPlayerHealthStatus()+ "%" + "\n");
+        Player1.setPlayerMaxHealth(100); // sets max health 
+        Player1.setPlayerCurrentHealth(100); //set current health 
+        System.out.println("Your current health points are " + Player1.getPlayerCurrentHealth()); //gets current health from instance of PlayerStats
+        System.out.println("of a max health points of " + Player1.getPlayerMaxHealth()); //gets Max health from instance of PlayerStats
+        System.out.println("Which is " + Player1.getPlayerHealthStatus()+ "%" + "\n"); //gets health status ((Current/Max)*100 + a percentage sign) from instance of PlayerStats
        
         System.out.println("Pick up sword? True or False");
-        boolean sword = keyboard.nextBoolean();
-        double swordWeight = 12;
-        if (sword = true)
-        Player1.gainItem(swordWeight);
+        boolean sword = keyboard.nextBoolean();// for testing
+        double swordWeight = 12; //sword weight for testing, not sure how to implement
+        if (sword = true) // if user takes sword
+        Player1.gainItem(swordWeight); //tells Player1 to gainItem with parameter swordWeight
         System.out.println("You are " + Player1.getPlayerWeightStaus() + "\n");
-        
+        // weight status tests if current weight is over max weight 
 		keyboard.close();
 	}
 }
