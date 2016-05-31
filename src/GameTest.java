55ad135173f241b7b8d35e3b67a66fc64cb0e2a8
connerkward <conner.k.ward@gmail.com>
2016-05-30 17:32:17 -0700

    Update GameTest.java

--- src/GameTest.java
+++ src/GameTest.java
@@ -12,7 +12,21 @@ public class GameTest {
 		System.out.println("What is your Carry Weight?");
         Player1.setPlayerCarryWeight((double) keyboard.nextDouble());
         System.out.println("Player Weight Status: ");
-        System.out.println(Player1.getPlayerWeightStaus());
+        System.out.println(Player1.getPlayerWeightStaus() + "\n");
+        
+        Player1.setPlayerMaxHealth(100);
+        Player1.setPlayerCurrentHealth(100);
+        System.out.println("Your current health points are " + Player1.getPlayerCurrentHealth());
+        System.out.println("of a max health points of " + Player1.getPlayerMaxHealth());
+        System.out.println("Which is " + Player1.getPlayerHealthStatus()+ "%" + "\n");
+       
+        System.out.println("Pick up sword? True or False");
+        boolean sword = keyboard.nextBoolean();
+        double swordWeight = 12;
+        if (sword = true)
+        Player1.gainItem(swordWeight);
+        System.out.println("You are " + Player1.getPlayerWeightStaus() + "\n");
+        
 		keyboard.close();
 	}
 }
