dd5328c7d3ea8f8b12f7636cf9f7b7b7c1b168fb
André Felipe Araujo Martins <andrearaujofm@gmail.com>
2016-06-05 13:28:36 -0700

    ITEMS FOR OUR SWEET WEIGHT SYSTEM

---
+++ src/Items.java
@@ -0,0 +1,20 @@
+public class Items {
+	//setting stuff up, need to fix to be able to import playerStats
+	String[] Type = {Weapon, Equipment, Accessory, Key, Consumable};
+	String Name;
+	String Description;
+	String Weight;
+	
+	/* Not sure if there's a better way, but by going this route
+	   every item would be it's own object. Not sure if this is effective
+	   but this is what I can come up with being only 5 chapters deep into 
+	   Headfirst
+	*/
+	
+	Key Item = new Key();
+	Item.Type = Key;
+	Item.Name = Dildo;
+	Item.Description = "A dildo is a sex toy, often explicitly phallic in appearance, intended for sexual penetration or other sexual activity during masturbation or with sex partners.";
+	Item.Weight = 10;
+	
+}
