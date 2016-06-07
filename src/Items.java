3671fdbfeb5476f2385d4e0f148a58f9a9beaac9
André Felipe Araujo Martins <andrearaujofm@gmail.com>
2016-06-07 12:32:32 -0700

    FUCK ME

--- src/Items.java
+++ src/Items.java
@@ -1,20 +1,18 @@
 public class Items {
-	//setting stuff up, need to fix to be able to import playerStats
-	String[] Type = {Accessory, Key, Consumable};
+	
+	String[] Type = {"Accessory", "Key", "Consumable"};
 	String Name;
 	String Description;
 	String Weight;
 	
-	/* Not sure if there's a better way, but by going this route
-	   every item would be it's own object. Not sure if this is effective
-	   but this is what I can come up with being only 5 chapters deep into 
-	   Headfirst
-	*/
+	Items [] I = new Item [999];
 	
-	Item Dildo = new Dildo();
-	.Type = Key;
-	.Name = Dildo;
-	.Description = "A dildo is a sex toy, often explicitly phallic in appearance, intended for sexual penetration or other sexual activity during masturbation or with sex partners.";
-	.Weight = 10;
+	// There has to be a better way to do this (Btw it's bugged as hell for some reason, I just can't get it to compile)
+	I[1] = new Item();
+	I[1].Type = "Key";
+	I[1].Name = "Basement Key";
+	I[1].Description = "Opens the church basement door";
+	I[1].Weight = "1";
+	
 	
 }
