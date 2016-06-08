e5a9aa4d3812e7a9feff3111348525c488525c1e
connerward <connerward@conners-mbp.lan>
2016-06-08 13:42:41 -0700

    conner push

--- src/Items.java
+++ src/Items.java
@@ -1,8 +1,8 @@
 // It fucking works
 
-class Item {
+class Items {
     enum Type {
-        ACCESSORY, KEY, CONSUMABLE
+        WEAPON, ACCESSORY, KEY, CONSUMABLE
     };
 
     Type itemType;
@@ -10,11 +10,11 @@ class Item {
     String itemDescription;
     String itemLongDescription;
     double itemWeight;
-
+		
     public static void main(String[] args) {
-        Item [] items = new Item [999];
+        Items [] items = new Items [999];
 
-        items[0] = new Item();
+        items[0] = new Items();
         items[0].itemType = Type.KEY;
         items[0].itemName = "Basement Key";
         items[0].itemDescription = "Opens the church basement door";
@@ -22,19 +22,27 @@ class Item {
         items[0].itemWeight = 0.1;
 		
 		
-		items[1] = new Item();
+		items[1] = new Items();
 		items[1].itemType = Type.CONSUMABLE;
 		items[1].itemName = "Conner's big fat cock";
 		items[1].itemDescription = "A dagger forged by the gods, of which can tear whole assholes apart";
         items[1].itemLongDescription = "Opens the church basement door";
 		items[1].itemWeight = 1000.1;
 		
-		items[2] = new Item();
+		items[2] = new Items();
 		items[2].itemType = Type.CONSUMABLE;
 		items[2].itemName = "Potion";
 		items[2].itemDescription = "A potion that heals 4 health.";
 		items[2].itemLongDescription = "Has a small 'Brewed by Bearwyn' label.";
-		items[2].itemWeight = 1000.1;
+		items[2].itemWeight = 10.0;
 		
+		items[3] = new Items();
+		items[3].itemType = Type.WEAPON;
+		items[3].itemName = "A 14 Pound Sword";
+		items[3].itemDescription = "A potion that weighs 14 pounds.";
+		items[3].itemLongDescription = "Strangely, it has the words 'DIE ANTWOORD' ingraved into its hilt.";
+		items[3].itemWeight = 14.0;
+		
+	    
     }
 }
