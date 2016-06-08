9c49cc03481bf805a0e7983c25ba3e0f3b87a1e2
connerward <connerward@conners-mbp.lan>
2016-06-08 13:26:02 -0700

    con update

--- src/Items.java
+++ src/Items.java
@@ -24,15 +24,16 @@ class Item {
 		
 		items[1] = new Item();
 		items[1].itemType = Type.CONSUMABLE;
-		items[1].itemName = "Conner's big fat penis";
+		items[1].itemName = "Conner's big fat cock";
 		items[1].itemDescription = "A dagger forged by the gods, of which can tear whole assholes apart";
-        items[0].itemLongDescription = "Opens the church basement door";
+        items[1].itemLongDescription = "Opens the church basement door";
 		items[1].itemWeight = 1000.1;
 		
 		items[2] = new Item();
 		items[2].itemType = Type.CONSUMABLE;
 		items[2].itemName = "Potion";
-		items[2].itemDescription = "Has a small 'Brewed by Bearwyn' label.";
+		items[2].itemDescription = "A potion that heals 4 health.";
+		items[2].itemLongDescription = "Has a small 'Brewed by Bearwyn' label.";
 		items[2].itemWeight = 1000.1;
 		
     }
