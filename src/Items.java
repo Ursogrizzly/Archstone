cfb62c841249ea5659ba9d103bf0c5c07bfbe193
André Felipe Araujo Martins <andrearaujofm@gmail.com>
2016-06-08 12:33:16 -0700

    GOD, IT'S BEAUTIFUL

--- src/Items.java
+++ src/Items.java
@@ -18,6 +18,13 @@ class Item {
         items[0].name = "Basement Key";
         items[0].description = "Opens the church basement door";
         items[0].weight = 0.1;
-	
+		
+		
+		items[1] = new Item();
+		items[1].type = Type.CONSUMABLE;
+		items[1].name = "Conner's big fat penis";
+		items[1].description = "A dagger forged by the gods, of which can tear whole assholes apart";
+		items[1].weight = 1000.1;
+		
     }
 }
